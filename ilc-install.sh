c0baeaedfdb97394f4b01767cedea684f4f44c3a
tomekw <tomek@w.pl>
2018-01-28 19:41:29 +0100

    ilc-install

--- ilc-install.sh
+++ ilc-install.sh
@@ -11,9 +11,6 @@ if sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; then echo "apt in use, exitin
 
 sudo apt-get update
 
-git clone https://github.com/iLCSoft/iLCInstall.git
-cd iLCInstall/
-
 sudo apt-get -y install git curl ocaml golang
 sudo apt-get -y install build-essential cmake subversion libmysqlclient-dev freeglut3-dev zlib1g-dev libqt4-dev cernlib-core-dev
 sudo apt-get -y install default-jdk libxpm-dev libxmu-dev doxygen latex2html
@@ -23,3 +20,14 @@ sudo apt-get -y install gfortran libssl-dev libpcre3-dev xlibmesa-glu-dev libgle
 sudo apt-get -y install libpythia8-dev liblz4-dev liblzma-dev libpng-dev libgif-dev libtiff5-dev libjpeg-dev libfreetype6-dev
 sudo apt-get -y install libxft-dev davix-dev libgfal2-dev libtbb-dev libpcre3-dev libgl2ps-dev libz3-dev libctypes-ocaml-dev
 
+
+
+git clone https://github.com/iLCSoft/iLCInstall.git
+cd iLCInstall/
+
+wget https://raw.githubusercontent.com/twto/ilc-install/master/release-versions-v01-19.py
+cp release-versions-v01-19.py releases/v01-19/release-versions.py
+
+
+./ilcsoft-install releases/v01-19/release-base.cfg -i
+./ilcsoft-install releases/v01-19/release-ilcsoft.cfg -i
